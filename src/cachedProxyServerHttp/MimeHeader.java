fc799638cba8ba93115090d0c1b25af9f52c51ef
gopika <gopika.jitesh2004@gmail.com>
2026-01-29 11:35:37 +0530

    MIME header implementation - passing attribute pairs between HTTP client and server (replaced with JSON formatting in modern browsers)

--- src/cachedProxyServerHttp/MimeHeader.java
+++ src/cachedProxyServerHttp/MimeHeader.java
@@ -1,3 +1,5 @@
+
+
 package cachedProxyServerHttp;
 import java.util.*;
 
@@ -8,6 +10,54 @@ class MimeHeader extends Hashtable {
 		  String s = st.nextToken();
 		  int colon = s.indexOf(':') ;;
 		  String key = s.substring(0,colon);
+		  String val = s.substring(colon+2);
+		  put(key,val);
 	  }
+  }
+  
+  //cnstrctr
+  MimeHeader(){}
+  MimeHeader(String d){
+	  parse(d);
+  }
+  
+  public String toString() {
+	  //each entry in the table built here
+	  String ret = "";
+	  Enumeration<?> e = keys();
+	  
+	  while(e.hasMoreElements()) {
+		  String key = (String) e.nextElement();
+		  String val = (String) get(key);
+		  ret +=key + ": "+val +"\r\n";
+	  }
+	  
+	  return ret;
+  }
+  
+  /* we are converting the parsed string here to canonical frm to ensure no caps or format mismatch*/
+  private String fix(String ms) {
+  char chars[] = ms.toLowerCase().toCharArray();
+  boolean upcase = true; 
+  
+   for(int i=0;i<chars.length-1;i++) {
+	   char ch=chars[i];
+	   if(upcase && 'a' <= ch && ch<='z') {
+		   chars[i] = (char) (ch-('a'-'A'));
+	   }
+	   upcase=(ch=='-');
+   }
+   return new String(chars);
+   
+  
+}
+  @Override
+  public String get(String key) {
+	  return (String) super.get(fix(key)); //override hashtable parent
+  }
+  
+  @Override
+  public void put(String key, String val) {
+	  super.put(fix(key),val); 
   }
 }
