fe9819e01e4898a3b2f443c751aa0546377bc9f2
gopika <gopika.jitesh2004@gmail.com>
2026-01-29 11:08:24 +0530

    commit

--- src/cachedProxyServerHttp/MimeHeader.java
+++ src/cachedProxyServerHttp/MimeHeader.java
@@ -6,7 +6,7 @@ class MimeHeader extends Hashtable {
 	  StringTokenizer st = new StringTokenizer(data, "\r\n");
 	  while(st.hasMoreTokens()) {
 		  String s = st.nextToken();
-		  int colon = s.indexOf(':');
+		  int colon = s.indexOf(':') ;;
 		  String key = s.substring(0,colon);
 	  }
   }
