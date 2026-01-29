fae0838d2fa09232ec6acac462b9a624cea0f020
gopika <gopika.jitesh2004@gmail.com>
2026-01-29 10:46:24 +0530

    simple caching proxy server : java- the complete reference

---
+++ src/cachedProxyServerHttp/MimeHeader.java
@@ -0,0 +1,13 @@
+package cachedProxyServerHttp;
+import java.util.*;
+
+class MimeHeader extends Hashtable {
+  void parse(String data) {
+	  StringTokenizer st = new StringTokenizer(data, "\r\n");
+	  while(st.hasMoreTokens()) {
+		  String s = st.nextToken();
+		  int colon = s.indexOf(':');
+		  String key = s.substring(0,colon);
+	  }
+  }
+}
